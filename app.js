ce26febd5723773cde76eba50f1a9d7d7e564b78
Phillip <degenaarp@gmail.com>
2020-11-19 20:19:01 +0200

    added login and token

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const MongoClient = require('mongodb').MongoClient;
 
 app.use(cors());
@@ -19,7 +21,7 @@ MongoClient.connect(URI, function(err, db) {
 });
 
 app.get('/', (req, res) => {
-    res.send("YOU ARE HEREasdasdasd");
+    res.send("Welcome to the classification API. Please make sure you are logged in");
 })
 
 app.get('/users', (req, res) => {
@@ -28,6 +30,78 @@ app.get('/users', (req, res) => {
         res.status(200).send(result);
     });
 })
+
+//Authenticate user
+async function authUser(theEmail, thePassword) {    
+    try{
+        const user  = await locateUser(theEmail);
+        return user;
+        
+    }catch(error){
+        console.log(error)
+    }    
+}; 
+
+async function locateUser(theEmail) {  
+   const user = await dbo.collection("Users").findOne({email: theEmail});
+   return user;
+}
+
+//user login
+app.post('/login', async (req, res) => {
+    //Authenticate user
+    const user = await authUser(req.body.email, req.body.password)
+
+    if(user !== null)
+    {
+        try{
+            if(await bcrypt.compare(req.body.password, user.password)){
+                console.log(user);
+                jwt.sign({user: user}, 'secretkey', { expiresIn: '10s'}, (err, token) => {
+                    //redirect to homepage
+                    res.json({
+						token,
+						username: user.username,
+                        message: "Success"
+                    })
+                });            
+            }else{
+                res.send('Not allowed');
+            }
+        }
+
+        catch{
+        res.status(500).send();
+        }       
+    }
+    else{
+        res.send('Cannot find user')
+    }
+    
+});
+
+//Verify token
+function verifyToken(req, res, next){
+    //Get auth header value
+    //We want ot send the token in the header
+    const bearerHeader = req.headers['authorization'];
+    //Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined'){
+        //Split at the space
+        const bearer = bearerHeader.split(' ');
+        //Get token from array
+        const bearerToken = bearer[1];
+        //Set the token
+        req.token = bearerToken;
+
+        //Call next middleware
+        next();
+    }
+    else{
+        //Forbidden
+        res.sendStatus(403);
+    }
+}
 
 app.listen(PORT, () => {
     console.log("Listening on port " + PORT)
