29ce12e5af270a556f54985f335fda0216a7a066
Phillip <degenaarp@gmail.com>
2020-11-19 20:15:24 +0200

    added mongo in declare

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
 
 app.use(cors());
 const PORT = process.env.PORT || 3000;
