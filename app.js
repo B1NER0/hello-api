60c30a52ccb15f6d2894f99bae1c6a257d7f1c16
Phillip <degenaarp@gmail.com>
2020-11-22 21:21:36 +0200

    added clear to constructor

--- app.js
+++ app.js
@@ -448,6 +448,7 @@ app.get('/getClientData/:id', (req, res) => {
 app.listen(PORT, '0.0.0.0' , () => {
     console.log("Listening on port with " + PORT)
 
+    clearUploads();
     
     MongoClient.connect(URI, function(err, db) {
         if(err) throw err;
